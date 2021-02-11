1087b0e41b2e6a3842c75e36941453a4b87844fb
Lennard Beers <lennard.beers@rub.de>
2021-02-11 11:31:16 +0100

    build fix + editor assembly updated

--- Assets/_Project/BlobbyEditor/Build.cs
+++ Assets/_Project/BlobbyEditor/Build.cs
@@ -1,23 +1,25 @@
 using System.IO;
 using UnityEditor;
 
-namespace Blobby
+namespace BlobbyEditor
 {
     public static class Build
     {
+        [MenuItem("Build/Build Client")]
         public static void BuildClient()
         {
             var path = Path.Combine(Directory.GetCurrentDirectory(), "Builds");
-            var levels = new[] {"Assets/Scenes/ClientScene.unity"};
+            var levels = new[] {"Assets/_Project/Scenes/ClientScene.unity"};
 
             BuildPipeline.BuildPlayer(levels, path + "/client.exe", BuildTarget.StandaloneWindows,
                 BuildOptions.None);
         }
 
+        [MenuItem("Build/Build Linux Server")]
         public static void BuildServerLinux()
         {
             var path = Path.Combine(Directory.GetCurrentDirectory(), "Builds");
-            var levels = new[] {"Assets/Scenes/ServerScene.unity"};
+            var levels = new[] {"Assets/_Project/Scenes/ServerScene.unity"};
 
             BuildPipeline.BuildPlayer(levels, path + "/server.exe", BuildTarget.StandaloneLinux64,
                 BuildOptions.EnableHeadlessMode);
