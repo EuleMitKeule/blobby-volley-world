10b8c4ac29f11e087b44997dbbdef5253d1fbd99
Lennard Beers <47421406+EuleMitKeule@users.noreply.github.com>
2021-02-13 14:35:02 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-chmod 777 /root/blobby-volley-world-server/blobby-volley-world-server-linux.x86_64
-xvfb-run --auto-servernum --server-args='-screen 0 640x480x24:32' /root/blobby-volley-world-server/blobby-volley-world-server-linux.x86_64 -batchmode -nographics
+chmod 777 /root/blobby-volley-world-server/blobby-volley-world-server-linux
+xvfb-run --auto-servernum --server-args='-screen 0 640x480x24:32' /root/blobby-volley-world-server/blobby-volley-world-server-linux -batchmode -nographics
