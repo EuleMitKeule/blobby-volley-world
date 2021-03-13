e6a9eebc47a8621308ecceb542bc2663480f5b12
Lennard Beers <47421406+EuleMitKeule@users.noreply.github.com>
2021-03-13 15:08:04 +0100

    ci fix

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 chmod 777 /root/blobby-volley-world-server/blobby-volley-world-server-linux
 rm /root/blobby-volley-world-server/serverData.json
-echo {"Name":"BV Server","Host":"127.0.0.1","Port":$LISTEN_PORT,"Token":"bla","MasterServerHost":"$MASTER_SERVER_HOST","MasterServerPort":15940} >> /root/blobby-volley-world-server/serverData.json
+echo "{\"Name\":\"BV Server\",\"Host\":\"127.0.0.1\",\"Port\":$LISTEN_PORT,\"Token\":\"bla\",\"MasterServerHost\":\"$MASTER_SERVER_HOST\",\"MasterServerPort\":15940}" >> /root/blobby-volley-world-server/serverData.json
 xvfb-run --auto-servernum --server-args='-screen 0 640x480x24:32' /root/blobby-volley-world-server/blobby-volley-world-server-linux -batchmode -nographics
