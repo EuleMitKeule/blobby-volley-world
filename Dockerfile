a1239f6804ee0d4b5c960a72084d18a12afcffb5
Lennard Beers <l.beers@outlook.de>
2022-12-04 12:15:41 +0100

    fix: add env to entrypoint

--- Dockerfile
+++ Dockerfile
@@ -5,7 +5,9 @@ ARG DOCKER_VERSION=17.06.0-ce
 
 ENV LISTEN_HOST="0.0.0.0"
 ENV LISTEN_PORT="1337"
-ENV MASTER_SERVER_HOST="bvmaster.eulenet.eu"
+ENV MASTER_SERVER_HOST="147.185.221.180"
+ENV MASTER_SERVER_PORT="19994"
+ENV MASTER_SERVER_TOKEN="bla"
 
 RUN apt-get update && \
 apt-get install -y libglu1 xvfb libxcursor1
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 chmod 777 /root/blobby-volley-world-server/blobby-volley-world-server-linux
 rm /root/blobby-volley-world-server/serverData.json
-echo "{\"Name\":\"BV Server\",\"Host\":$LISTEN_HOST,\"Port\":$LISTEN_PORT,\"Token\":\"bla\",\"MasterServerHost\":\"$MASTER_SERVER_HOST\",\"MasterServerPort\":15940}" >> /root/blobby-volley-world-server/serverData.json
+echo "{\"Name\":\"$SERVER_NAME\",\"Host\":$LISTEN_HOST,\"Port\":$LISTEN_PORT,\"Token\":\"$MASTER_SERVER_TOKEN\",\"MasterServerHost\":\"$MASTER_SERVER_HOST\",\"MasterServerPort\":$MASTER_SERVER_PORT}" >> /root/blobby-volley-world-server/serverData.json
 xvfb-run --auto-servernum --server-args='-screen 0 640x480x24:32' /root/blobby-volley-world-server/blobby-volley-world-server-linux -batchmode -nographics
